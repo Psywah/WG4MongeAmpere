ff1a643e821661644eaf72d00c50c204623eb80a
Psywah <psywah@gmail.com>
2018-09-09 17:04:03 -0400

    Gong edited

--- test2.m
+++ test2.m
@@ -10,9 +10,9 @@ h=1;
 %% number of unknows
 [~,edge,bdDof] = dofP2(elem);
 Ndof = size(node,1)+size(edge,1)*5 +size(elem,1) - numel(bdDof)- 2*sum(double(bdDof>size(node,1)));
-showmesh(node,elem);
-findnode(node);
-findedge(node,edge);
+%showmesh(node,elem);
+%findnode(node);
+%findedge(node,edge);
 
 %% sourse function
 f = @(coord) sin(pi * coord(:,1)).* sin(pi*coord(:,2)); 
