53849bf1ff18a4ac6d4d6e9c5acc27a29fa68fd0
Psywah <psywah@gmail.com>
2018-09-09 20:10:52 -0400

    Gong edited

--- test2.m
+++ test2.m
@@ -10,9 +10,9 @@ h=1;
 %% number of unknows
 [~,edge,bdDof] = dofP2(elem);
 Ndof = size(node,1)+size(edge,1)*5 +size(elem,1) - numel(bdDof)- 2*sum(double(bdDof>size(node,1)));
-%showmesh(node,elem);
-%findnode(node);
-%findedge(node,edge);
+showmesh(node,elem);
+findnode(node);
+findedge(node,edge);
 
 %% sourse function
 f = @(coord) sin(pi * coord(:,1)).* sin(pi*coord(:,2)); 
@@ -25,7 +25,7 @@ F = WG4MongeAmpere(x, elem, node,f,h) ;
 fun = @(x) WG4MongeAmpere(x, elem, node,f,h) ;
 x0 = rand(Ndof,1);
 options = optimoptions('fsolve','Algorithm','levenberg-marquardt',...
-    'Display','iter','MaxIter',1000,'MaxFunEvals',10000);
+    'Display','iter','MaxIter',100,'MaxFunEvals',1000);
 
 x = fsolve(fun,x0,options)
 F = fun(x)
