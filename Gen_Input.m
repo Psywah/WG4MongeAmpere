bf7ce6d6c35b8b7e53c33527dd2b2e90559acd3e
Psywah <psywah@gmail.com>
2019-01-03 15:00:04 -0500

    Gong edited

--- Gen_Input.m
+++ Gen_Input.m
@@ -1,82 +1,82 @@
 y=[];
 NN=length(x);
-for i=1:NN
-    y=[y;sym(['y', num2str(i)])];
-end
-fid=fopen('input','wt');
-
-%% Configure part
-fprintf(fid,'CONFIG\n');
-
-fprintf(fid,'MPTYPE:0;\n');
-fprintf(fid,'SecurityLevel:1;\n');
-fprintf(fid,'FINALTOL: 1e-16;\n');
-fprintf(fid,'AMPMaxPrec: 2048;\n');
-
-fprintf(fid,'END;\n\n');
-
-%% Inputs
-fprintf(fid,'INPUT\n');
-
-ff=['function '];
-for j=1:NN
-    if j==NN
-        ff=[ff 'f',num2str(j)];
-    else
-        ff=[ff 'f',num2str(j),','];
-    end
-end
-
-variable=['variable_group '];
-for j=1:NN-5
-    variable=[variable,char(y(j)),','];
-end
-variable=[variable,char(y(NN-4))];
-fprintf(fid,[variable,';\n']);
-
-variable=['variable_group '];
-for j=NN-3:NN-1
-    variable=[variable,char(y(j)),','];
-end
-variable=[variable,char(y(NN))];
-fprintf(fid,[variable,';\n']);
-
-fprintf(fid,[ff,';\n']);
-
-%F=WG4MongeAmpere_Input(y, elem, node,f,h);
-F = WG4MongeAmpere_Input(y, elem, node,h,f,u,ux,uy) ;
-
-for j=1:NN
-    
-    ftmp = ['f',num2str(j), ' = ',char(vpa(F(j))),';\n'];
-    fprintf(fid,ftmp);
-    
-end
-
-fclose(fid);
+% for i=1:NN
+%     y=[y;sym(['y', num2str(i)])];
+% end
+% fid=fopen('input','wt');
 % 
-% unix('./bertini1 input')
+% %% Configure part
+% fprintf(fid,'CONFIG\n');
 % 
-% fid=fopen('real_finite_solutions','r');
-% num=fscanf(fid,'%e',1);
-% x=[];
-% for i=1:num
-%     xtmp=[];
-%     for j=1:NN
-%         xtmp(j)=fscanf(fid,'%e',1);
-%         tmp=fscanf(fid,'%e',1);
-%     end
-%     xtmp=xtmp';
-%     isinx=0;
-%     for j=1:size(x,2)
-%         if norm(x(:,j)-xtmp)<1e-10
-%             isinx=1;
-%             break
-%         end
-%     end
-%     if ~isinx
-%         x=[x xtmp];
+% fprintf(fid,'MPTYPE:0;\n');
+% fprintf(fid,'SecurityLevel:1;\n');
+% fprintf(fid,'FINALTOL: 1e-16;\n');
+% fprintf(fid,'AMPMaxPrec: 2048;\n');
+% 
+% fprintf(fid,'END;\n\n');
+% 
+% %% Inputs
+% fprintf(fid,'INPUT\n');
+% 
+% ff=['function '];
+% for j=1:NN
+%     if j==NN
+%         ff=[ff 'f',num2str(j)];
+%     else
+%         ff=[ff 'f',num2str(j),','];
 %     end
 % end
+% 
+% variable=['variable_group '];
+% for j=1:NN-5
+%     variable=[variable,char(y(j)),','];
+% end
+% variable=[variable,char(y(NN-4))];
+% fprintf(fid,[variable,';\n']);
+% 
+% variable=['variable_group '];
+% for j=NN-3:NN-1
+%     variable=[variable,char(y(j)),','];
+% end
+% variable=[variable,char(y(NN))];
+% fprintf(fid,[variable,';\n']);
+% 
+% fprintf(fid,[ff,';\n']);
+% 
+% %F=WG4MongeAmpere_Input(y, elem, node,f,h);
+% F = WG4MongeAmpere_Input(y, elem, node,h,f,u,ux,uy) ;
+% 
+% for j=1:NN
+%     
+%     ftmp = ['f',num2str(j), ' = ',char(vpa(F(j))),';\n'];
+%     fprintf(fid,ftmp);
+%     
+% end
+% 
 % fclose(fid);
-% save(['./data/ele', int2str(4), '_square1.mat'],'x');
+% % 
+% % unix('./bertini1 input')
+% 
+fid=fopen('real_finite_solutions','r');
+num=fscanf(fid,'%e',1);
+x=[];
+for i=1:num
+    xtmp=[];
+    for j=1:NN
+        xtmp(j)=fscanf(fid,'%e',1);
+        tmp=fscanf(fid,'%e',1);
+    end
+    xtmp=xtmp';
+    isinx=0;
+    for j=1:size(x,2)
+        if norm(x(:,j)-xtmp)<1e-10
+            isinx=1;
+            break
+        end
+    end
+    if ~isinx
+        x=[x xtmp];
+    end
+end
+fclose(fid);
+save(['./data/ele', int2str(4), '_square1.mat'],'x');
--- demo_twogrid.m
+++ demo_twogrid.m
@@ -1,7 +1,7 @@
 
 clear
 %% define mesh size
- h=1; Nbisect = 3;
+ h=1; Nbisect = 1;
  h = h/2^(Nbisect/2);
 %  f = @(coord) 4*ones(size(coord(:,1)));
 % ug = @(coord) 4*zeros(size(coord(:,1)));
@@ -23,7 +23,7 @@ clear
 
 
 % u = x^4+y^4 + px^2y^2
-p=10;
+p=1;
 f = @(coord) (144-12*p^2)*coord(:,1).^2.*coord(:,2).^2 + 24*p*(coord(:,2).^4+coord(:,1).^4);
 u = @(coord) coord(:,1).^4 +coord(:,2).^4 + p*coord(:,1).^2.*coord(:,2).^2 ;
 ux = @(coord) 4*coord(:,1).^3 + 2*p*coord(:,1).*coord(:,2).^2;
@@ -101,16 +101,16 @@ tol = 1e-6;
 for i = 1:NinitSol
 %     [x, F,EXITFLAG,OUTPUT,JACOB] = fsolve(fun,x0(:,i),options);
 %    cond(JACOB)
-%    [x, F, EXITFLAG] = fsolve(fun,x0(:,i),options);
-    x=newton_bertini(elemn, noden,h,f,u,ux,uy,x0(:,i));
+    [x, F, EXITFLAG] = fsolve(fun,x0(:,i),options);
+%    x=newton_bertini(elemn, noden,h,f,u,ux,uy,x0(:,i));
     if size(x,1) > 1
         F= fun(x);
         ini_error = norm(fun(x0(:,i)));
         normF = norm(F);
         fprintf('solution %d, resid %e, init resid%e\n',i,normF,ini_error);
-        %if normF<tol || normF/ini_error< tol %|| EXITFLAG >0
+        if normF<tol || normF/ini_error< tol || EXITFLAG >0
          allx=[allx,x];
-        %end
+        end
     end
 end
 
